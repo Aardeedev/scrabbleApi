7a964d2f153d64d279dc42f80b02ba388163e841
Aardeedev <dave.james.adams@gmail.com>
2019-12-16 23:22:30 +0000

    Make fetch from scrabble service

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
+const dotenv = require('dotenv');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const { log, ExpressAPILogMiddleware } = require('@rama41222/node-logger');
-const scrabbleScore = require('./scrabble-score.js').score;
+const axios = require('axios');
 
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config();
+}
 const config = {
-  name: 'sample-express-app',
+  name: 'scrabble-api',
   port: 3000,
   host: '0.0.0.0',
+  SCORE_ADDRESS: process.env.SCORE_ADDRESS || 'localhost',
+  SCORE_PORT: process.env.SCORE_PORT || 3001,
 };
 
 const app = express();
@@ -18,18 +24,24 @@ app.use(cors());
 app.use(ExpressAPILogMiddleware(logger, { request: true }));
 
 app.get('/', (req, res) => {
-  res.status(200).send('Scrabble score is running');
+  res.status(200).send('Scrabble Api is running');
 });
 
-app.get('/scrabbleScore/:word', (req, res, err) => {
+app.get('/scrabbleScore/:word', async (req, res, err) => {
   const word = req.params.word;
-  const score = scrabbleScore(word);
-  res.json({ payload: { score } });
+  try {
+    const score = await axios.get(
+      `http://${config.SCORE_ADDRESS}:${config.SCORE_PORT}/${word}`
+    );
+    res.json(score.data);
+  } catch (err) {
+    res.json({ message: 'call to service scrabble hs failed', payload: err });
+  }
 });
 
-app.listen(config.port, config.host, e => {
+app.listen(config.port, e => {
   if (e) {
     throw new Error('Internal Server Error');
   }
-  logger.info(`${config.name} running on ${config.host}:${config.port}`);
+  logger.info(`${config.name} running on port ${config.port}`);
 });
